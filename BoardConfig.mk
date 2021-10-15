994c8240cb424ec4783625ccf67f0072b9eb7ddf
Vaisakh Murali <mvaisakh@statixos.com>
2021-10-15 16:19:04 +0600

    mido: Allow copying over of prebuilt ELF binaries
    With commit [1], the build system restricts copying over prebuilt ELF
    binaries. Since most of our blobs are proprietary, we ship them as
    binaries, hence requiring this bypass to succeed with build.
    
    Set BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES to true.
    
    [1] https://github.com/StatiXOS/android_build/commit/0b49c03727cab24cb89d3da6b89bc79fdcbef45f
    
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Change-Id: I58e4c87106d960e1684a84b6edbdd7acb7f2222b
    Reviewed-on: https://review.statixos.com/c/android_device_xiaomi_cannon/+/5852
    Reviewed-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Tested-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Signed-off-by: Hunter-commits <rutujamandlik06@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,6 +16,10 @@
 
 DEVICE_PATH := device/xiaomi/mido
 
+# Build restriction bypass
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
