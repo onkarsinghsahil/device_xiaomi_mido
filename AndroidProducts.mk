d36a3427c0a89284a8d8e9853bdaf01db33f9cfe
Alone0316 <70204635+Hunter-commits@users.noreply.github.com>
2021-10-26 21:01:13 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/radiant_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    radiant_mido-user \
+    radiant_mido-userdebug \
+    radiant_mido-eng
