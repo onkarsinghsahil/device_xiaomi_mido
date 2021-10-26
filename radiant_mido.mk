d29ddae31a2f7eeb29ef165fd994950677dc4af1
Alone0316 <70204635+Hunter-commits@users.noreply.github.com>
2021-10-26 21:00:08 +0530

    Update and rename lineage_mido.mk to radiant_mido.mk

--- radiant_mido.mk
+++ radiant_mido.mk
@@ -26,7 +26,7 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
-PRODUCT_NAME := lineage_mido
+PRODUCT_NAME := radiant_mido
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 4
 PRODUCT_MANUFACTURER := Xiaomi
