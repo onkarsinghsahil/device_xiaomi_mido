cddc3ab8d34e66c3bc7c27e1863635480b9fcaf3
Alone0316 <70204635+Hunter-commits@users.noreply.github.com>
2021-10-27 07:18:56 +0530

    Update radiant_mido.mk

--- radiant_mido.mk
+++ radiant_mido.mk
@@ -21,8 +21,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mido device
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Radiant stuff.
+$(call inherit-product, vendor/radiant/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
