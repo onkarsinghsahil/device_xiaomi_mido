156e043e997b10e0a7f0a05a79609bbbc9ff11cc
Hunter-commits <rutujamandlik06@gmail.com>
2021-11-26 14:46:36 +0000

    Make android.hardware.secure_element@1.0.vendor
    Signed-off-by: Hunter-commits <rutujamandlik06@gmail.com>

--- device.mk
+++ device.mk
@@ -354,6 +354,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.0.vendor\
     librmnetctl \
     libcnefeatureconfig \
     qti-telephony-hidl-wrapper \
