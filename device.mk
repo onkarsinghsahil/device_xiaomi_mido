9582deed8dfa47c861ca46dd1c1a9e443f304bac
shubhojitnag <shubhojitnag2009@gmail.com>
2021-12-23 14:06:44 +0530

    mido : Build android.hardware.drm@1.3.vendor

--- device.mk
+++ device.mk
@@ -206,6 +206,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3.vendor \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
