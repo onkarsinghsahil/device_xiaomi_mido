739a4e77d41fc5ef509fcf8e94c504e6bbae2bac
Karan Parashar <whyredfire@gmail.com>
2021-12-25 19:30:22 +0530

    violet: Import apn config - for some reason apn config doesn't get copied from vendor/aosp - from https://github.com/PixelOS-Pixelish/vendor_aosp/blob/twelve/prebuilt/common/etc/apns-conf.xml
    Signed-off-by: Karan Parashar <whyredfire@gmail.com>

--- device.mk
+++ device.mk
@@ -109,6 +109,10 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.1 \
     android.hardware.neuralnetworks@1.1.vendor
 
+# APN
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/etc/apns-conf.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/apns-conf.xml
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
