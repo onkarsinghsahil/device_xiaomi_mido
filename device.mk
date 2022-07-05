b3b81456deaa402cc3fc2c8f81ee13195adcceea
onkarsinghsahil <onkarsinghsahil@gmail.com>
2022-07-05 12:27:50 +0000

    mido: overlay: set voltageos maintainer string
     - Add maintainer overlay
     - Needed for VoltageOS settings to show maintainer name else will show as unknown maintainer
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Onkar Singh <onkarsinghsahil@gmail.com>

--- device.mk
+++ device.mk
@@ -25,6 +25,7 @@ PRODUCT_COMPRESSED_APEX := false
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-voltage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
